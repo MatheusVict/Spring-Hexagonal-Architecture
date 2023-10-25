8ce9404d7765bed19254acd84d35feb241595abe
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-25 13:11:16 -0300

    feat: inital commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/test/java/io/matheusvictor/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
